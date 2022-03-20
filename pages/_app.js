42f135f28d53b8f010f6ac09b7d141ce8a290450
Himanshu Rai <raihimanshu22345gmail.com>
2022-03-20 17:41:40 +0530

    mui added

---
+++ pages/_app.js
@@ -0,0 +1,50 @@
+import { SessionProvider } from "next-auth/react";
+
+import React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ToastContainer } from "react-toastify";
+import theme from "../theme";
+import Layout from "../components/Layout";
+
+export default function MyApp(props) {
+  const {
+    Component,
+    pageProps: { session, ...pageProps },
+  } = props;
+
+  React.useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <SessionProvider session={session}>
+          {/* <ToastContainer /> */}
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </SessionProvider>
+        <CssBaseline />
+      </ThemeProvider>
+    </React.Fragment>
+  );
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
